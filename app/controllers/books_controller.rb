bf1b9e3a89ab6b11d66b165548c0141a02a2f424
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-04-27 14:05:28 +0000

    bookers-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
   end
-
+  
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+  
+  def destroy
+  end
+  
+  def update
+  end
+  
+  def create
+    book = Book.new
+    book.save
+    redirect_to book_path(book.id)
+  end
+
 end
