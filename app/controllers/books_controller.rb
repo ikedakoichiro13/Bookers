47b7ba2359a50f545e83fa1a1456109f217c2fbf
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-04-28 13:56:59 +0000

    bookers-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,12 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
 
   def edit
   end
@@ -20,10 +26,9 @@ class BooksController < ApplicationController
   def update
   end
   
-  def create
-    book = Book.new
-    book.save
-    redirect_to book_path(book.id)
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
