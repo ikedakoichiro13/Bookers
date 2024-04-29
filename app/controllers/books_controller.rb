1216fe3ad1d7a40aa29aad569b4872e6bc511433
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-04-29 14:03:51 +0000

    bookers-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
@@ -18,14 +18,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
   def destroy
   end
-  
+
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  resources :books
+  resources :books do
+    member do
+    patch:update
+  end
+  end
+  
   #get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
