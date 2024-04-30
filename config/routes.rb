4c438b26c99c3b3535e064852819287c5632b729
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-04-30 14:06:03 +0000

    bookers-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  resources :books do
-    member do
-    patch:update
-  end
-  end
-  
+  resources :books
+
   #get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
