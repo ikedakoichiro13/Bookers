c1d6e8ecbc1c6f0099e01afa04bfa2499f10dc9f
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-05-02 14:23:15 +0000

    bookers-7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+     @books = Book.all
+     render :index
+    end
   end
 
   def edit
@@ -24,12 +29,14 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+  　book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
